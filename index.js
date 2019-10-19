d0b3a72a31a7085893eee9f61a13f2995b58ef1d
BenLucht <benjamin.b.lucht@gmail.com>
2019-10-19 22:31:41 +0200

    Handle login to website

--- index.js
+++ index.js
@@ -15,6 +15,27 @@ const scrape = async () => {
 
     await page.goto('https://www.datacamp.com/users/sign_in');
 
+    // type in username
+    await page.type('#user_email', usr);
+
+    // click "next"-button and wait for website feedback
+    await Promise.all([
+        page.click('.js-account-check-email'),
+        page.waitForSelector('#user_password', { visible: true, }),
+    ]);
+
+    // type in password
+    await page.type('#user_password', pwd);
+
+    // click "sign in"-button and wait for redirect page to load
+    await Promise.all([
+        page.click('.js-modal-submit-button'),
+        page.waitForNavigation({ waitUntil: 'networkidle2' }),
+    ]);
+
+    // show url that was redirected to
+    console.log('redirected to: ' + page.url());
+
     // make screenshot of where we are
     await page.screenshot({path: 'screenshot.png'});
 
