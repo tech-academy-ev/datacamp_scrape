ad2c1071a9452bcaff69e407f85e5b10c78f501c
BenLucht <benjamin.b.lucht@gmail.com>
2019-10-25 23:48:59 +0200

    Split loading leaderboard into getLeaderboard

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ const timeout = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const scrape = async () => {
-    const browser = await puppeteer.launch();
-    
-    const page = await browser.newPage();
-
+const getLeaderboard = async (page) => {
     // to prevent mobile view
     await page.setViewport({width: 1200, height: 720});
 
     await page.goto('https://www.datacamp.com/users/sign_in');
 
+    console.log('datacamp loaded');
+
     // type in username
     await page.type('#user_email', usr);
+
+    console.log('username entered');
 
     // click "next"-button and wait for website feedback
     await Promise.all([
@@ -31,11 +31,22 @@ const scrape = async () => {
     // type in password
     await page.type('#user_password', pwd);
 
+    console.log('password entered')
+
     // click "sign in"-button and wait for redirect page to load
     await Promise.all([
         page.click('.js-modal-submit-button'),
-        page.waitForNavigation({ waitUntil: 'networkidle2' }),
+        //page.waitForNavigation({ waitUntil: 'networkidle2' }),
     ]);
+
+    try {
+        await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    } catch (error) {
+        console.log(error);
+    }
+
+    // wait some more to make sure
+    await timeout(5000);
 
     // show url that was redirected to
     console.log('redirected to: ' + page.url());
@@ -43,8 +54,12 @@ const scrape = async () => {
     // go to yearly leaderboard
     await page.goto('https://www.datacamp.com/enterprise/marketing-analytics-marketing-2/leaderboard/year');
 
+    console.log('leaderboard loading');
+
     // wait until table is actually loaded
     await page.waitForSelector('.dc-table__tr');
+
+    console.log('leaderboard loaded');
 
     // wait some more to make sure
     await timeout(5000);
@@ -52,14 +67,27 @@ const scrape = async () => {
     // get html of rendered site
     const content = await page.content();
 
-    // write html to file for further development (without pinging datacamp too much)
-    fs.writeFile("./leaderboard.html", content, (err) => {if(err){console.log('couldnt write')}});
-
     // make screenshot of where we are
     await page.screenshot({path: 'screenshot.png'});
 
+    console.log('leaderboard loaded');
+
+    return content
+}
+
+const scrape = async () => {
+    const browser = await puppeteer.launch({ "headless": false});
+    
+    const page = await browser.newPage();
+
+    const leaderboardContent = await getLeaderboard(page);
+
+    // write html to file for further development (without pinging datacamp too much)
+    //fs.writeFile("./leaderboard.html", content, (err) => {if(err){console.log('couldnt write')}});
+
     // close browser 
     await browser.close();
+
 }
 
 scrape();
