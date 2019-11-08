e595815542be2e9b7c6d0bc9d65cd3766a5678f7
BenLucht <benjamin.b.lucht@gmail.com>
2019-11-08 23:18:19 +0100

    Split off readProfile function

---
+++ readProfile.js
@@ -0,0 +1,19 @@
+const moment = require('moment');
+const parseProfile = require('./parseProfile');
+
+const readProfile = async (page, url) => {
+
+    await page.goto(url);
+    console.log(`profile ${url} loaded`);
+
+    const content = await page.content();
+
+    const urlParam = url.split('/');
+    const currDate = moment().format('YYYYMMDD');
+
+    fs.writeFile(`./profile_${urlParam[urlParam.length - 1]}_${currDate}.html`, content, (err) => {if(err){console.log('couldnt write')}});
+
+    parseProfile(false, content);
+}
+
+module.exports = readProfile;
